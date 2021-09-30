166701f854228df289bce547c432c2b98850cc78
Carla2320 <cguamanzara@est.ups.edu.ec>
2021-09-30 17:03:41 -0500

    Commit inicial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,352 @@
+<template>
+  
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>traffico | Landing, Responsive &amp; Business Templatee</title>
+
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico">
+    <link rel="manifest" href="assets/img/favicons/manifest.json">
+    <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
+    <meta name="theme-color" content="#ffffff">
+
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link href="assets/css/theme.css" rel="stylesheet" />
+
+  </head>
+  <body>
+  
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-4 d-block" data-navbar-on-scroll="data-navbar-on-scroll">
+        <div class="container"><a class="navbar-brand" href="index.html"><img src="assets/img/gallery/logo.png" height="24" alt="..." /></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"> </span></button>
+          <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base">
+              <li class="nav-item px-2" data-anchor="data-anchor"><a class="nav-link fw-medium active" aria-current="page" href="#home">Home</a></li>
+              <li class="nav-item px-2" data-anchor="data-anchor"><a class="nav-link" href="#aboutUs">Sobre nosotros</a></li>
+              <li class="nav-item px-2" data-anchor="data-anchor"><a class="nav-link" href="#clients">Servicios</a></li>
+        
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <section id="home">
+        <div class="bg-holder" style="background-image:url(assets/img/gallery/header-bg.png);background-position:center;background-size:cover;">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="container">
+          <div class="row align-items-center min-vh-50 min-vh-sm-100">
+            <div class="col-md-7 col-lg-6 text-md-start text-center">
+              <h1 class="text-light fs-6 fs-xl-8 font-cursive fw-normal mb-5">¿En mudanzas?<br />Cuidamos hasta<br />el último detalle.</h1><a class="btn btn-primary" href="#info" role="button">CONTÁCTANOS
+                <svg class="bi bi-arrow-right" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                </svg></a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section id="aboutUs">
+
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-6 text-center mb-6 mb-md-0 order-0 order-md-1"><img class="w-100" src="assets/img/gallery/about-us.png" alt="..." /></div>
+            <div class="col-md-6 text-center text-md-start mb-6">
+              <h4 class="text-danger">SOBRE NOSOTROS</h4>
+              <p class="my-5 fs-1 pe-xl-8">Nos apasionamos por ofrecer a las familias Ecuatorianas estrategias prácticas y oportunas, para un correcto desenvolvimiento en el mundo de mudanzas que generen satisfacción y economía en nuestros clientes.</p>
+              <div class="card card-span bg-soft-primary border-start border-primary border-5 mt-3">
+                <div class="card-body">
+                  <h4 class="lh-base px-lg-5 py-3">
+                    ¿No sabe por dónde empezar la mudanza? No se preocupe... ¡Nosotros lo hacemos más fácil para usted!
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section>
+
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-6 text-center mb-6 mb-md-0"><img class="w-100" src="assets/img/gallery/how-to-apply.png" alt="..." /></div>
+            <div class="col-md-6 text-center text-md-start mb-6 px-4">
+              <h4 class="text-danger text-uppercase">¿POR QUÉ PREFERIRNOS?</h4>
+              <p class="my-5 fs-1 pe-xl-7">Trabajamos las 24 horas del dia a nivel local y nacional; le garantizamos tranquilidad en la mudanza de su casa, departamento, oficina o empresa, sin que se preocupe por nada. Además contamos con camiones propios, alfombrados, mantas protectoras y materiales para proteger sus bienes; así como con un equipo humano de profesionales altamente calificados.</p>
+            </div>
+          </div>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+     
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section id="clients">
+
+        <div class="container">
+          <div class="row">
+            <div class="col-md-11 col-lg-6 col-xl-4 col-xxl-3">
+              <h4 class="text-danger text-uppercase">SERVICIOS</h4>
+              <h1 class="font-cursive fw-normal mb-5">CONOCE NUESTROS PLANES </h1>
+            </div>
+            <div class="col-xxl-9">
+              <div class="position-relative offset-9 offset-sm-10 offset-lg-11 mb-3"></div>
+              <div class="carousel slide" id="carouselExampleDark" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-bs-interval="10000">
+                    <div class="row h-100 mx-1 mb-5 mt-3 gx-md-2 gx-lg-4">
+                      <div class="col-md-4 mb-5 mb-md-0">
+                        <div class="card card-span h-100 shadow-primary">
+                          <div class="card-body my-3">
+                          <center><h1 class="font-cursive fw-normal mb-5 ">Mudanzas Básicas </h1></center>
+                            <p style="text-align: justify" class="mb-0 px-3 px-md-2 px-xxl-3">Pensando en la economía de nuestros clientes este tipo de mudanza es donde deben tener recogido los enseres de su hogar como es vajilla, ropa, calzado y todo lo relacionado con su mudanzas donde nuestro personal si le ayudaría al desmontaje y montaje de muebles.</p>
+                            <div class="align-items-xl-center d-block d-xl-flex px-3 mt-3 align-self-end">
+                              <a href="#info" class="btn btn-primary">CONTRÁTALO
+                        <svg class="bi bi-arrow-right ms-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                        </svg>
+                        </a>
+                      
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4 mb-5 mb-md-0">
+                        <div class="card card-span h-100 shadow-primary">
+                          <div class="card-body my-3">
+                          <center><h1 class="font-cursive fw-normal mb-5 ">Mudanzas Completas</h1></center>
+                            <p style="text-align: justify" class="mb-0 px-3 px-md-2 px-xxl-3">El embalaje y el empaque es una parte fundamental de nuestros servicios Pensando en nuestros clientes este tipo de mudanza es mas compleja donde nuestro personal se encarga de su mudanza completa. </p>
+                            <div class="align-items-xl-center d-block d-xl-flex px-3 mt-3 align-self-end">
+                              <div class="flex-1 align-items-center pt-2">
+                               <a href="#info" class="btn btn-primary">CONTRÁTALO
+                        <svg class="bi bi-arrow-right ms-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                        </svg>
+                        </a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      
+                      <div class="col-md-4 mb-5 mb-md-0">
+                        <div class="card card-span h-100 shadow-primary">
+                          <div class="card-body my-3">
+                          <center><h1 class="font-cursive fw-normal mb-5 ">Servicios Corporativos</h1></center>
+                            <p style="text-align: justify" class="mb-0 px-3 px-md-2 px-xxl-3">Ofrece un servicio corporativo e integral, contamos con Servicio de mudanzas de empresas, Mudanzas de Oficinas, es muy importante la planificación para que la operatividad de la empresa y el cliente no se vea interrumpida. </p>
+                            <div class="align-items-xl-center d-block d-xl-flex px-3 mt-3 align-self-end">
+                              <div class="flex-1 align-items-center pt-2">
+                               <a href="#info" class="btn btn-primary">CONTRÁTALO
+                        <svg class="bi bi-arrow-right ms-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                        </svg>
+                        </a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <center>
+                      <br/>
+                      <div class="col-md-4 mb-8 mb-md-0">
+                        <div class="card card-span h-100 shadow-primary">
+                          <div class="card-body my-3">
+                          <center><h1 class="font-cursive fw-normal mb-5 ">Mudanzas Especiales</h1></center>
+                            <p style="text-align: justify" class="mb-0 px-3 px-md-2 px-xxl-3"> En relación a las obras de arte y antigüedades y pianos ofrecemos servicio y empaques especialmente, de tal manera se minimice el riesgo de ser lastimadas durante el trayecto.</p>
+                            <div class="align-items-xl-center d-block d-xl-flex px-3 mt-3 align-self-end">
+                              <div class="flex-1 align-items-center pt-2">
+                                <a href="#info" class="btn btn-primary">CONTRÁTALO
+                        <svg class="bi bi-arrow-right ms-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                        </svg>
+                        </a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      </center>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+      <section id="info">
+        <div class="bg-holder" style="background-image:url(assets/img/gallery/footer-bg.png);background-position:left;background-size:contain;padding:5px 3px;">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="container">
+          <div class="row min-vh-lg-50 min-vh-xl-75">
+            <div class="col-lg-6 col-xl-5 my-2 py-9 d-none d-lg-block">
+              <div class="card card-span bg-transparent border-start border-primary border-5 mt-3">
+                <div class="card-body">
+                  <p class="text-light fs-2 lh-lg px-lg-3">Ponte en contacto con nosotros para compartir tus necesidades o para recibir una asesoría completa sobre nuestros servicios.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 col-xl-5 mb-3">
+              <div class="card card-span shadow py-4 px-5">
+                <div class="card-body">
+                  <form class="row g-0 align-items-center" ref="form" @submit.prevent="sendEmail" id="contact-form">
+                    <div class="mb-3">
+                      <label class="form-label" for="formGroupExampleInput">Nombres</label>
+                      <input class="form-control form-traffico-control" id="formGroupExampleInput" type="text" placeholder="Nombres" name="user_name" required/>
+                    </div>
+                    <div class="mb-3">
+                      <label class="form-label" for="formGroupExampleInput2">Correo Electrónico</label>
+                      <input class="form-control form-traffico-control" id="formGroupExampleInput2" type="text" placeholder=" Correo electrónico" name="user_email" required/>
+                    </div>
+                    <div class="mb-3">
+                      <label class="form-label" for="formGroupExampleInput2">Dirección</label>
+                      <input class="form-control form-traffico-control" id="formGroupExampleInput2" type="text" placeholder="Dirección/Sector" name="user_adress" required/>
+                    </div>
+                    <div class="col-12">
+                      <button class="btn btn-primary"  @click="testToast" @toggle-off="resetForm" type="submit">Enviar
+                        <svg class="bi bi-arrow-right ms-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                        </svg>
+                      </button>
+                    </div>
+                  </form>
+                   <transition name="fade" mode="out-in">
+    <div v-if="testButClicked" class="alert" role="alert">
+      Gracias por comunicarte con nosotros! Revisa tu email.
+    </div>
+  </transition>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section class="py-5">
+
+        <div class="container">
+          <div class="row flex-center">
+            <div class="col-md-6 col-xl-8 order-0">
+              <p class="text-center text-md-start">All rights Reserved &copy; Your Company, 2021</p>
+            </div>
+            <div class="col-md-6 col-xl-3 order-1">
+              <p class="text-center text-md-end text-xl-start">Made with&nbsp;
+                <svg class="bi bi-suit-heart-fill" xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="#EE4D47" viewBox="0 0 16 16">
+                  <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"></path>
+                </svg>&nbsp;by&nbsp;
+              </p>
+              <ul class="list-unstyled list-inline mb-6 mb-md-0 text-center text-md-end text-xl-start">
+                <li class="list-inline-item mr-2"><a class="text-decoration-none" href="https://www.instagram.com/"><img class="list-social-icon" src="assets/img/icons/i.png" alt="..." /></a></li>
+                <li class="list-inline-item mr-2"><a class="text-decoration-none" href="https://twitter.com/"><img class="list-social-icon" src="assets/img/icons/t.png" alt="..." /></a></li>
+                <li class="list-inline-item mr-2"><a class="text-decoration-none" href="https://www.facebook.com/"><img class="list-social-icon" src="assets/img/icons/f.png" alt="..." /></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+    </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+
+
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&amp;family=Rubik:wght@300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
+  </body>
+
+</template>
+
+<script>
+import emailjs from 'emailjs-com';
+
+export default {
+   data() {
+            return {
+                testButClicked: false
+            }
+        },
+
+  methods: {
+    sendEmail() {
+    //emailJS.com
+      emailjs.sendForm('service_7r37qw9', 'template_va6n0r1', this.$refs.form, 'user_5xJgBrUc7djWTJVv5eyHO')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+        }, (error) => {
+
+            console.log('FAILED...', error.text);
+        });
+    },
+    testToast() {
+                this.testButClicked = true;
+            }
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+
+</style>
